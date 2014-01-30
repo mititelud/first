48b7a2c0caced24b35350ab60baa8b895daa2858
dragos.mititelu <dragos.mititelu@ro.neusoft.com>
2014-01-30 09:59:09 +0200

    operator - operator * commit

--- main.cpp
+++ main.cpp
@@ -54,6 +54,34 @@ class Matrix
 			temp.afis();
 			return temp;
 		}
+		
+		Matrix operator -(Matrix &m)
+		{
+			Matrix<T,r,c> temp;
+			
+			for(int i=0;i<r;i++)
+				for(int j=0;j<c;j++)
+				{
+					temp.x[i][j]=x[i][j]-m.x[i][j];
+				}
+			cout<<"Overload -"<<endl;
+			temp.afis();
+			return temp;
+		}
+		
+		template<class T1, int r1, int c1>
+		Matrix<T, r, c1> operator *(Matrix<T1, r1, c1> &m)
+		{
+			Matrix<T,r,c1> temp;
+			
+			for(int i=0;i<r;i++)
+				for(int j=0;j<c1;j++)
+					for(int k=0;k<r1;k++)
+						temp.x[i][j]+=x[i][k]*m.x[k][j];
+			cout<<"Overload *\n";
+			//temp.afis();
+			return temp;
+		}
 };
 
 int main()
@@ -64,5 +92,13 @@ int main()
 	y.init(20);
 	x.afis();
 	y.afis();
+	Matrix<int,2,3>a;
+	Matrix<int,3,5> b;
+	a.init(9);
+	b.init(11);
 	x+y;
+	a.afis();
+	b.afis();
+	Matrix<int,2,5> r=a*b;
+	r.afis();
 }
